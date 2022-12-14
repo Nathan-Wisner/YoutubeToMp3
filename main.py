8fdf133e58aa5155aed210bdd57ba9cf3bd93677
Nathan Wisner <wisner.nathan@gmail.com>
2022-12-13 20:37:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import moviepy.editor as mp
 import os
 
-src = r"C:\Users\Nathan\Documents\youtube-dl-downloads\music\good"
-dest = r"C:\Users\Nathan\Documents\youtube-dl-downloads\music\good"
+src = r"C:\Users\%USERPROFILE%\Documents\YTDownloader\Videos"
+dest = r"C:\Users\%USERPROFILE%\Documents\YTDownloader\MP3"
 
 for filename in os.listdir(src):
     if filename.endswith(".mp4") and not os.path.exists(src + "\\" + filename[:-4] + ".wav"):
