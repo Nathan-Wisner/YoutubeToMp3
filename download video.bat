353ecfc19afe9585bb369cb9da2b1c835e1a9262
Nathan Wisner <wisner.nathan@gmail.com>
2022-12-13 20:37:52 -0800

    Update download video.bat

--- download video.bat
+++ download video.bat
@@ -1,3 +1,3 @@
 set /p url="Paste input url or id: "
-youtube-dl -o "C:\%HOMEPATH%\Documents\youtube-dl-downloads\videos\%%(title)s.%%(ext)s" %url%
+youtube-dl -o "C:\%HOMEPATH%\Documents\YTDownloader\Videos\%%(title)s.%%(ext)s" %url%
 pause
